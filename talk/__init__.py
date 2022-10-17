6abc755e3f86e1b06d3d5defb2e3048fadeea084
Andreas Poehlmann <andreas@poehlmann.io>
2022-10-17 23:17:28 +0200

    talk: add DATA_DIR variable

--- talk/__init__.py
+++ talk/__init__.py
@@ -2,7 +2,10 @@
 just a few definitions to make the introduction slide more fun
 """
 from collections import namedtuple
+from pathlib import Path
 
 Degree = type("Degree", (), dict(__init__=lambda *_: None))
 Career = type("Career", (), dict(__init__=lambda *_: None))
 Session = namedtuple("Session", "name")
+
+DATA_DIR = Path(__file__).parent.parent
